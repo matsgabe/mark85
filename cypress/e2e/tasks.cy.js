ec767b2d73bcb2d9a29e8029567f3c685208b356
matsgabe <matsgabe@gmail.com>
2026-01-26 14:57:47 -0300

    add new validation in tasks tests

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -11,11 +11,16 @@ describe("POST /tasks", () => {
     cy.task("deleteUser", user.email);
     cy.postUser(user);
 
-    cy.postSession(user).then((response) => {
+    cy.postSession(user).then((userResp) => {
       cy.task("deleteTask", task.name, user.email);
 
-      cy.postTask(task, response.body.token).then((response) => {
+      cy.postTask(task, userResp.body.token).then((response) => {
         expect(response.status).to.eq(200);
+        expect(response.body.name).to.eq(task.name);
+        expect(response.body.tags).to.eql(task.tags);
+        expect(response.body.is_done).to.be.false;
+        expect(response.body.user).to.be.eq(userResp.body.user._id);
+        expect(response.body._id.length).to.eq(24);
       });
     });
   });
